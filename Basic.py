a66ad52ae04d75337e8ae8fae5a9432a783ca71c
Saloni paney-ux <workspaceworkout0716@gmail.com>
2026-09-21 14:55:03 +0530

    basic learner

---
+++ Basic.py
@@ -0,0 +1,11 @@
+# comment this is comment hahahah
+#single line comment #is using here multi line comment is not there but we can achieve it by using docstring """ ..dfuidfhuisdh """" this    
+
+#variable 
+ironMan = "Tony Stark" #camelcase
+#pascal case
+BlackWidow="heehe"
+
+#type
+a=10
+print(type(a)) 
